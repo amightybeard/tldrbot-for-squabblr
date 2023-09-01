ff158f6dca42f96fb39db519c1368fcf705aec5c
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-09-01 11:56:24 -0400

    Update tldrbot.py

--- tldrbot.py
+++ tldrbot.py
@@ -119,7 +119,10 @@ def extract_content_with_bs(url):
     """
     Extracts main content of an article using BeautifulSoup
     """
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Extract title
