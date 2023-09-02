ce9181a43984c0138063a9e03e8818389bad478b
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-09-02 16:10:04 -0400

    Update tldrbot.py

--- tldrbot.py
+++ tldrbot.py
@@ -3,7 +3,7 @@ import os
 import json
 import logging
 import io
-import csv
+# import csv
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from transformers import BartForConditionalGeneration, BartTokenizer
@@ -16,8 +16,9 @@ logging.basicConfig(level=logging.INFO)
 SQUABBLES_TOKEN = os.environ.get('SQUABBLES_TOKEN')
 GIST_TOKEN = os.environ.get('GITHUB_TOKEN')
 GIST_ID = os.environ.get('TLDRBOT_GIST')
-FILE_NAME = 'tldrbot-timestamp.csv'
+FILE_NAME = 'tldrbot-processed-ids.json'
 CSV_PATH = 'includes/communities.csv'
+GIST_URL = f"https://gist.githubusercontent.com/amightybeard/{GIST_ID}/raw/{FILE_NAME}"
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -62,73 +63,107 @@ def post_reply(post_id, summary):
 
 from io import StringIO
 
-def get_last_timestamp(community_name):
-    """
-    Get the last timestamp for a specific community from the CSV in the Gist.
-    """
-    logging.info(f"Fetching last timestamp for community: {community_name}")
+# def get_last_timestamp(community_name):
+#     """
+#     Get the last timestamp for a specific community from the CSV in the Gist.
+#     """
+#     logging.info(f"Fetching last timestamp for community: {community_name}")
     
-    # Use the raw URL to fetch the file content directly
-    raw_gist_url = f"https://gist.githubusercontent.com/amightybeard/{GIST_ID}/raw/{FILE_NAME}"
-    response = requests.get(raw_gist_url)
-    if response.status_code != 200:
-        logging.error(f"Failed to fetch CSV from Gist. Status code: {response.status_code}")
-        return None
+#     # Use the raw URL to fetch the file content directly
+#     raw_gist_url = f"https://gist.githubusercontent.com/amightybeard/{GIST_ID}/raw/{FILE_NAME}"
+#     response = requests.get(raw_gist_url)
+#     if response.status_code != 200:
+#         logging.error(f"Failed to fetch CSV from Gist. Status code: {response.status_code}")
+#         return None
 
-    csv_content = response.text  # Get the CSV content directly
+#     csv_content = response.text  # Get the CSV content directly
     
-    # Log a snippet of the fetched CSV content
-    logging.info(f"CSV content snippet: {csv_content[:100]}...")
+#     # Log a snippet of the fetched CSV content
+#     logging.info(f"CSV content snippet: {csv_content[:100]}...")
 
-    # Begin search in CSV content
-    logging.info(f"Searching CSV for timestamp of community: {community_name}")
+#     # Begin search in CSV content
+#     logging.info(f"Searching CSV for timestamp of community: {community_name}")
 
-    reader = csv.DictReader(io.StringIO(csv_content))
-    for row in reader:
-        if row['Community'] == community_name:
-            return datetime.strptime(row['Post Created Date'], DATE_FORMAT)
+#     reader = csv.DictReader(io.StringIO(csv_content))
+#     for row in reader:
+#         if row['Community'] == community_name:
+#             return datetime.strptime(row['Post Created Date'], DATE_FORMAT)
     
-    # Community not found in the CSV
-    logging.warning(f"Community '{community_name}' not found in the CSV.")
-    return None
+#     # Community not found in the CSV
+#     logging.warning(f"Community '{community_name}' not found in the CSV.")
+#     return None
 
-def save_last_timestamp(community, post_url, timestamp):
-    logging.info(f"Saving last timestamp for community: {community}")
+def fetch_last_processed_ids():
+    try:
+        resp = requests.get(GIST_URL)
+        resp.raise_for_status()
+        data = resp.json()
+        return data
+    except requests.RequestException as e:
+        logging.error(f"Failed to fetch processed IDs from Gist. Error: {e}")
+        return {}
+
+# def save_last_timestamp(community, post_url, timestamp):
+#     logging.info(f"Saving last timestamp for community: {community}")
     
-    # First, fetch the current content of the CSV from the Gist
-    resp = requests.get(f'https://api.github.com/gists/{GIST_ID}', headers={'Authorization': f'token {GIST_TOKEN}'})
-    data = resp.json()
-    print(f"GitHub API Response: {data}")
-    # Extract the CSV content from the Gist response
-    csv_content = data['files'][FILE_NAME]['content']
+#     # First, fetch the current content of the CSV from the Gist
+#     resp = requests.get(f'https://api.github.com/gists/{GIST_ID}', headers={'Authorization': f'token {GIST_TOKEN}'})
+#     data = resp.json()
+#     print(f"GitHub API Response: {data}")
+#     # Extract the CSV content from the Gist response
+#     csv_content = data['files'][FILE_NAME]['content']
 
-    # Append the new entry to the CSV content
-    new_entry = f"\n{community},{post_url},{timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')}"
-    updated_csv_content = csv_content + new_entry
+#     # Append the new entry to the CSV content
+#     new_entry = f"\n{community},{post_url},{timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')}"
+#     updated_csv_content = csv_content + new_entry
 
-    # Update the Gist with the new CSV content
-    gist_update_data = {
+#     # Update the Gist with the new CSV content
+#     gist_update_data = {
+#         "files": {
+#             FILE_NAME: {
+#                 "content": updated_csv_content
+#             }
+#         }
+#     }
+#     update_resp = requests.patch(f'https://api.github.com/gists/{GIST_ID}', headers={'Authorization': f'token {GIST_TOKEN}'}, json=gist_update_data)
+#     if update_resp.status_code == 200:
+#         print(f"Successfully updated the timestamp for {community} in the Gist.")
+#     else:
+#         print(f"Failed to update the Gist. Response code: {update_resp.status_code}. Response text: {update_resp.text}")
+
+def save_processed_id(community, post_id):
+    data = fetch_last_processed_ids()
+    data[community]['last_processed_id'] = post_id
+    
+    headers = {
+        'Authorization': f'token {GIST_ID}',
+        'Accept': 'application/vnd.github.v3+json',
+        'Content-Type': 'application/json'
+    }
+    
+    payload = {
         "files": {
             FILE_NAME: {
-                "content": updated_csv_content
+                "content": json.dumps(data, indent=4)
             }
         }
     }
-    update_resp = requests.patch(f'https://api.github.com/gists/{GIST_ID}', headers={'Authorization': f'token {GIST_TOKEN}'}, json=gist_update_data)
-    if update_resp.status_code == 200:
-        print(f"Successfully updated the timestamp for {community} in the Gist.")
-    else:
-        print(f"Failed to update the Gist. Response code: {update_resp.status_code}. Response text: {update_resp.text}")
+
+    try:
+        resp = requests.patch(f"https://api.github.com/gists/{GIST_ID}", headers=headers, json=payload)
+        resp.raise_for_status()
+    except requests.RequestException as e:
+        logging.error(f"Failed to update processed ID for {community}. Error: {e}")
 
 def read_domain_blacklist(filename="includes/blacklist-domains.txt"):
     with open(filename, 'r') as file:
         blacklist = [line.strip() for line in file]
     return blacklist
 
-def read_word_blacklist(filename="includes/blacklist-words.txt"):
-    with open(filename, 'r') as file:
-        blacklist = [line.strip() for line in file]
-    return blacklist
+# def read_word_blacklist(filename="includes/blacklist-words.txt"):
+#     with open(filename, 'r') as file:
+#         blacklist = [line.strip() for line in file]
+#     return blacklist
 
 def extract_content_with_bs(url):
     """
@@ -244,61 +279,99 @@ def get_summary(url):
         logging.error(f"Error in generating summary for URL: {url}. Error: {e}")
         return None
 
-def get_latest_posts(username, community):
-    last_timestamp = get_last_timestamp(community) or datetime.min
-    data = requests.get(f'https://squabblr.co/api/s/{community}/posts?page=1&sort=new&').json()
-    logging.info(f"Checking posts for community: {community}")
+# def get_latest_posts(username, community):
+#     last_timestamp = get_last_timestamp(community) or datetime.min
+#     data = requests.get(f'https://squabblr.co/api/s/{community}/posts?page=1&sort=new&').json()
+#     logging.info(f"Checking posts for community: {community}")
 
-    domain_blacklist = read_domain_blacklist()
-    logging.info(f"Loaded domain blacklist: {domain_blacklist}")
+#     domain_blacklist = read_domain_blacklist()
+#     logging.info(f"Loaded domain blacklist: {domain_blacklist}")
 
-    for post in data.get('data', []):
-        created_at = post['created_at']
-        post_date = datetime.strptime(created_at, DATE_FORMAT)
+#     for post in data.get('data', []):
+#         created_at = post['created_at']
+#         post_date = datetime.strptime(created_at, DATE_FORMAT)
 
-        # Check if url_meta exists and is not None
-        url_meta = post.get("url_meta")
-        if not url_meta:
-            logging.warning(f"Post with ID: {post['hash_id']} lacks 'url_meta'. Skipping.")
+#         # Check if url_meta exists and is not None
+#         url_meta = post.get("url_meta")
+#         if not url_meta:
+#             logging.warning(f"Post with ID: {post['hash_id']} lacks 'url_meta'. Skipping.")
+#             continue
+
+#         # Check post type and skip if it's an image
+#         post_type = url_meta.get("type")
+#         if post_type == "image":
+#             logging.info(f"Skipping post with ID: {post['hash_id']} as it is an image.")
+#             continue
+
+#         if post_date > last_timestamp:
+#             post_id = post['hash_id']
+#             logging.info(f"New post found with ID: {post_id} and date: {post_date}")
+#             if post.get("url_meta"):
+#                 url = post["url_meta"].get("url")
+#                 post_domain = urlparse(url).netloc
+#                 if post_domain in domain_blacklist:
+#                     continue  # Skip this post if its domain is blacklisted
+#                 if url:
+#                     summary = get_summary(url)
+
+#                     # word_blacklist = read_word_blacklist()
+#                     # for word in word_blacklist:
+#                         # summary = summary.replace(word, "")  # Remove blacklisted words/phrases from the summary
+
+#                     if summary:
+#                         # Convert the summary into bullet points
+#                         response = f"{canned_message_header}\n\n{summary}\n\n{canned_message_footer}"
+#                         r = post_reply(post_id, response)
+#                         if r:
+#                             print("Posted: " + post_id)
+#                             # Save the post details to the CSV
+#                             save_last_timestamp(community, post["url"], post_date)
+#                         else:
+#                             print(post)
+#                             print("Failed Posting: " + post_id)
+#             else:
+#                 logging.info(f"No new post since the last timestamp: {last_timestamp}")
+
+def get_latest_posts():
+    processed_ids = fetch_last_processed_ids()
+
+    for community, data in processed_ids.items():
+        last_processed_id = data['last_processed_id']
+
+        logging.info(f"Checking posts for community: {community}")
+        response = requests.get(f'https://squabblr.co/api/community/{community}/posts')
+
+        if response.status_code != 200:
+            logging.warning(f"Failed to fetch posts for community: {community}")
             continue
 
-        # Check post type and skip if it's an image
-        post_type = url_meta.get("type")
-        if post_type == "image":
-            logging.info(f"Skipping post with ID: {post['hash_id']} as it is an image.")
-            continue
+        posts = response.json()
+        for post in posts:
+            post_id = post['id']
 
-        if post_date > last_timestamp:
-            post_id = post['hash_id']
-            logging.info(f"New post found with ID: {post_id} and date: {post_date}")
-            if post.get("url_meta"):
-                url = post["url_meta"].get("url")
-                post_domain = urlparse(url).netloc
-                if post_domain in domain_blacklist:
-                    continue  # Skip this post if its domain is blacklisted
-                if url:
-                    summary = get_summary(url)
+            if post_id <= last_processed_id:
+                continue
 
-                    # word_blacklist = read_word_blacklist()
-                    # for word in word_blacklist:
-                        # summary = summary.replace(word, "")  # Remove blacklisted words/phrases from the summary
+            logging.info(f"New post found with ID: {post_id} in community: {community}")
 
-                    if summary:
-                        # Convert the summary into bullet points
-                        response = f"{canned_message_header}\n\n{summary}\n\n{canned_message_footer}"
-                        r = post_reply(post_id, response)
-                        if r:
-                            print("Posted: " + post_id)
-                            # Save the post details to the CSV
-                            save_last_timestamp(community, post["url"], post_date)
-                        else:
-                            print(post)
-                            print("Failed Posting: " + post_id)
-            else:
-                logging.info(f"No new post since the last timestamp: {last_timestamp}")
+            url = post["url"]
+            if is_valid_domain(url):
+                content = extract_content_with_bs(url)
+                summary = generate_summary(content)
+                if summary:
+                    r = post_reply(post['hash_id'], summary)
+                    if r.get('success', False):
+                        logging.info(f"Successfully posted a reply for post ID: {post['hash_id']}")
+                        save_processed_id(community, post_id)
+                    else:
+                        logging.warning(f"Failed to post a reply for post ID: {post['hash_id']}.")
+
+# if __name__ == "__main__":
+#     with open(CSV_PATH, mode='r') as file:
+#         csv_reader = csv.DictReader(file)
+#         for row in csv_reader:
+#             get_latest_posts(row['Username'], row['Community'])
 
 if __name__ == "__main__":
-    with open(CSV_PATH, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            get_latest_posts(row['Username'], row['Community'])
+    logging.basicConfig(level=logging.INFO)
+    get_latest_posts()
